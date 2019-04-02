408a83f13d56b64f630dc3f58fbd43b67cf7b632
Ashley Gerdes <ashley.gerdes@uon.edu.au>
2019-04-02 02:08:53 +0000

    added the choice of encrypting and decrypting for both cyphers

--- Cypher1.c
+++ Cypher1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-char str;
+char str1;
+char str2;
 
 const char* encryptR(const char* a);
 const char* encryptS(const char* a);
@@ -13,15 +14,37 @@ int main()
     
     printf("Which cypher would you like to use?\n a) Rotation Cypher\n b) Substitution Cypher\n");
     
-    scanf("%c", &str);
+    scanf("%s", &str1);
     
-    if(str == 'a'){
+    if(str1 == 'a'){
     
         printf("Would you like to:\n a) Encrypt message\n b) Decrypt message");
         
-    }else if(str == 'b'){
+        scanf("%s", &str2);
+        
+        if(str2 == 'a'){
+            printf("ENCRYPT R\n");
+        }else if(str2 == 'b'){
+            printf("DECRYPT R\n");
+        }else{
+            printf("ERROR");
+        }
+        
+    }else if(str1 == 'b')
+    {
     
         printf("Would you like to:\n a) Encrypt message\n b) Decrypt message");
+        
+        scanf("%s", &str2);
+        
+        printf("after scan\n");
+        if(str2 == 'a'){
+            printf("ENCRYPT S\n");
+        }else if(str2 == 'b'){
+            printf("DECRYPT S\n");
+        }else{
+            printf("ERROR");
+        }
         
     }else{
         printf("ERROR");
