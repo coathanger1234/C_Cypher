0351a81ea39307271b63ba563c22e146d4ef1430
Ashley Gerdes <ashley.gerdes@uon.edu.au>
2019-04-09 02:41:58 +0000

    test

--- Cypher1.c
+++ Cypher1.c
@@ -3,56 +3,76 @@
 char str1;
 char str2;
 
-const char* encryptR(const char* a);
-const char* encryptS(const char* a);
-const char* decryptR(const char* a);
-const char* decryptS(const char* a);
+void encryptR();
+//void encryptS();
+void decryptR();
+//void decryptS();
 
 
 int main()
-{ 
-    
+{
+    FILE *Input, *Output;
+    FILE *input = fopen("Input","r");
     printf("Which cypher would you like to use?\n a) Rotation Cypher\n b) Substitution Cypher\n");
-    
     scanf("%s", &str1);
-    
     if(str1 == 'a'){
-    
-        printf("Would you like to:\n a) Encrypt message\n b) Decrypt message");
-        
+        printf("Would you like to:\n a) Encrypt message\n b) Decrypt message\n");
         scanf("%s", &str2);
-        
         if(str2 == 'a'){
-            printf("ENCRYPT R\n"); 
+            encryptR(); 
         }else if(str2 == 'b'){
-            printf("DECRYPT R\n");
+            decryptR();
         }else{
             printf("ERROR");
-        }
-        
-    }else if(str1 == 'b')
-    {
-    
+        }   
+    }
+}
+
+    /*else if(str1 == 'b'){
         printf("Would you like to:\n a) Encrypt message\n b) Decrypt message");
-        
         scanf("%s", &str2);
-        
         printf("after scan\n");
         if(str2 == 'a'){
-            printf("ENCRYPT S\n");
+            printf("ENCRYPT S\n"){
         }else if(str2 == 'b'){
             printf("DECRYPT S\n");
         }else{
-            printf("ERROR");
+            printf("ERROR");    
         }
-        
     }else{
-        printf("ERROR");
-        
+        printf("ERROR");       
     }
     return 0;
 }
-
-/*const char* encryptR(const char* a){
+*/
+void encryptR(){    
+    FILE *Input, *Output;
+    FILE *input = fopen("Input","r");
     
-}*/
+    char string[1024];
+    
+    if(input == NULL){ 
+        perror("fopen()");
+        return;
+    }else{
+        while(!feof(input)){
+            fscanf(input, "%s", string);
+            
+            int i = 0;
+            
+            printf("%s\n", string);
+        }
+    }
+    
+}
+//void encryptS(){
+//    String word[] = "a";
+//}
+void decryptR(){
+    char word[] = "b";
+    int i = 0;
+    printf("%c\n", word[i]);
+}
+//void decryptS(){
+//    String word[] = "a";
+//}
--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 # C_Cypher
 Assignment for C
-Running in Che:
-(ENTER IN TERMINAL)
-
-  cd c_Cypher/
-  gcc Cypher1.c
-  ./a.out
-  
-  
