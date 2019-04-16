e2961758e1f8f42562da69e757beb28024cf2195
Ashley Gerdes <ashley.gerdes@uon.edu.au>
2019-04-16 23:57:00 +0000

    CeaserCypher finally works, except for negative numbers, hopefully will fix that in the future, will now get started on the Sub Cypher
    The way my ceaser cypher works is by individually changing the characters of the sentence, unsure of why using negative numbers doesnt work tho,

--- CeaserCypher.c
+++ CeaserCypher.c
@@ -2,42 +2,106 @@
 
 int main()
 {
-    char String[1024] ="ZZZZZZZZZZZZZZ";
+    int r;
+    char String[1024] ="We Strike At Dawn";
     printf("%s\n",String);
-    for(int i = 0; String[i] != 0 ;i++){
-        char letter = String[i];
-        letter =+ 2;
-        
-        
-        /*if(String[i] > 64 && String[i] < 123){
-            if(String[i] < 91||String[i] > 96){
-                letter++; 
+    printf("Rotations: \n");
+    scanf("%d", &r);
+    printf("%d\n", r);
+    if(r > 0){
+        char letter;
+        //printf("%d",r);
+        for(int p = 0; p < r; p++){
+            for(int i = 0; String[i] != 0 ;i++){
+                letter = String[i];
+                letter++;
+                
+                
+                /*if(String[i] > 64 && String[i] < 123){
+                    if(String[i] < 91||String[i] > 96){
+                        letter++; 
+                    }
+                }else{
+                    continue;
+                }*/
+                
+                switch(letter){
+                    case 64:
+                        String[i] = 90;
+                        break;
+                    case 91:
+                        String[i] = 65;
+                        break;
+                    case 96:
+                        String[i] = 122;
+                        break;
+                    case 122:
+                        String[i] = 97;
+                        break;
+                    case 33:
+                        String[i] = 32;
+                        break;
+                    default:
+                        String[i] = letter;
+                        break;
+                }
+                
+                //printf("%c", letter);
+                //printf("%c\n",String[i]);
             }
-        }else{
-            continue;
-        }*/
-        
-        switch(letter){
-            case 64:
-                letter = 90;
-                break;
-            case 91:
-                letter = 65;
-                break;
-            case 96:
-                letter = 122;
-                break;
-            case 122:
-                letter = 97;
-                break;
-            case 33:
-                letter = 32;
-                break;
-            default:
-                break;
+            //printf("%c", letter);
         }
-        printf("%c", letter);
-        //printf("%c\n",String[i]);
+        for(int i = 0; String[i] != 0; i++){
+            printf("%c", String[i]);
+        }
+    }if(r < 0){
+        char letter;
+        //printf("%d",r);
+        for(int p = 0; p < r; p--){
+            for(int i = 0; String[i] != 0 ;i++){
+                letter = String[i];
+                letter--;
+                
+                
+                /*if(String[i] > 64 && String[i] < 123){
+                    if(String[i] < 91||String[i] > 96){
+                        letter++; 
+                    }
+                }else{
+                    continue;
+                }*/
+                
+                switch(letter){
+                    case 64:
+                        String[i] = 90;
+                        break;
+                    case 91:
+                        String[i] = 65;
+                        break;
+                    case 96:
+                        String[i] = 122;
+                        break;
+                    case 122:
+                        String[i] = 97;
+                        break;
+                    case 33:
+                        String[i] = 32;
+                        break;
+                    default:
+                        String[i] = letter;
+                        break;
+                }
+                
+                //printf("%c", letter);
+                //printf("%c\n",String[i]);
+            }
+            //printf("%c", letter);
+        }
+        for(int i = 0; String[i] != 0; i++){
+            printf("%c", String[i]);
+        }
     }
+
+
     return 0;
 }
