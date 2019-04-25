28a329766385f16fe18e6adc26e637450481cfba
Ashley Gerdes <ashley.gerdes@uon.edu.au>
2019-04-25 02:05:35 +0000

    SubCypher can now change lower case letters to upper case

--- SubCypher2.c
+++ SubCypher2.c
@@ -5,7 +5,7 @@ int main(){
     // = "ABCDEFGHIJKLMNOPQRWSTUVWXYZ"
     char alphabet2[1024] = "QWERTYUIOPASDFGHJKLZXCVBNM";
     int b = 0;
-    char String[1024] = "This Message Has Some CAPITALS";
+    char String[1024] = "ONLY PRINT this";
     char String2[1024];
     char choiceSwap;
     char choice;
@@ -31,17 +31,19 @@ int main(){
 
     for(int i = 0; String[i] != 0; i++){
         if(String[i] <= 122 && String[i] >= 97){
-            String[i] - 32;
+            String[i] = String[i] - 32;
+            //printf("%c", String[i]);
         }
-        /*for(int x = 0; alphabet[x] != 0; x++){
+        
+        for(int x = 0; alphabet[x] != 0; x++){
             if(String[i] == alphabet[x]){
-                printf("%i:%i   MATCH\n",i,x);
+                //printf("%i:%i   MATCH\n",i,x);
                 String2[i] = alphabet2[x];
             }else if(String[i] == 32){
                 String2[i] = 32;
             }
-        }*/
-       printf("%s\n",String);
+        }
+       //printf("%s\n",String);
     }
     printf("%s\n",String2);
 
